1b1785c266cf1fa940b38010decca8144961c8be
John Culpin <john@johnculpin.co.uk>
2017-11-02 20:00:19 +0000

    Remove hard-coded path from template

--- argument.py
+++ argument.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import sys
+import os
 filename  = sys.argv[1][0:-3]
 print (filename)
 splitname, spliextension = sys.argv[1].split('.')
 print (splitname)
 print (spliextension)
+print (os.getcwd())
